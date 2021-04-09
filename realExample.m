3b0e82090f2c63d82f67ed9028528896a006f660
Junhao Wang <54256124+jwang44@users.noreply.github.com>
2021-04-09 10:20:17 +0800

    Add files via upload

---
+++ realExample.m
@@ -0,0 +1,20 @@
+% Real-world example, corresponding to the first part of 2.4.2
+clear; close
+x = audioread('source_Muss_l.wav'); x = x(:,1);
+% 2000 samples of audio file sampled at 44100
+x = x(1001:3000);
+subplot(2,1,1); plot(x)
+
+% assume maximum length of a burst
+Nmax = 50;
+
+% estimate AR parameters
+p = 3*Nmax + 2;
+[A, e] = aryule(x, p);  % 1,a1,a2...
+
+% compute detection function d
+% d(t) = 1*x(t)+a1*x(t-1)+a2*x(t-2)...+ap*x(t-p)
+d = filter(A, 1, x);
+d(1:p) = d(1:p)*0;  % d is only defined for t>p
+d = abs(d);
+subplot(2,1,2); plot(d)
---
+++ singleBursts.m
@@ -0,0 +1,67 @@
+% Variant1: for single bursts, only detect the first and last sample
+% that exceeds the threshold, and assume the samples in between are all
+% noise. 
+% This is an simple improvement on direct thresholding and only works well
+% when there is only one burst in the audio.
+% corresponding to 2.4.1 in the paper
+clear; close
+s = audioread('acousticg.wav'); s = s(:,1);
+% 2000 samples of audio file sampled at 44100
+s = s(1:2000);
+subplot(3,1,1); plot(s)
+
+% gaussian noise with a certain variance and length
+variance = 10^-3;
+Nmax = 50;
+n = sqrt(variance)*randn(Nmax, 1);
+
+% add the noise to the middle of the signal
+x = s;
+nOnset = round((length(s)-Nmax)/2);
+x(nOnset:nOnset+Nmax-1) = x(nOnset:nOnset+Nmax-1)+n;
+subplot(3,1,2); plot(x)
+
+% estimate AR parameters
+p = 3*Nmax + 2;
+[A, e] = aryule(x, p);  % 1,a1,a2...
+
+% compute detection function d
+% d(t) = 1*x(t)+a1*x(t-1)+a2*x(t-2)...+ap*x(t-p)
+d = filter(A, 1, x);
+d(1:p) = d(1:p)*0;  % d is only defined for t>p
+d = abs(d);
+subplot(3,1,3); plot(d)
+
+% detect impulsive noise with direct thresholding
+K = 0:10^-4:12;
+% K = 0:10^-2:12;
+thresholds = K*sqrt(e); % e is estimated variance of excitation
+ps = zeros(size(thresholds));
+rs = zeros(size(thresholds));
+i = zeros(size(x)); i(nOnset:nOnset+Nmax-1) = 1; % true noisy region (label)
+for m=1:length(thresholds)
+    thre = thresholds(m);
+%     thre = thresholds(length(thresholds)-m+1);
+    pos = d>=thre;
+    % get the first and last sample of detected noise
+    k = find(pos);
+    firstSample = min(k);
+    lastSample = max(k);
+    % all samples in between are assumed to belong to the noise
+    pos(firstSample:lastSample) = 1;
+    neg = ones(size(d)) - pos;
+%     neg = d<thre;
+    tp = sum(pos&i);
+    tn = sum(neg&(~i));
+    fp = sum(pos&(~i));
+    fn = sum(neg&i);
+    precision = tp/(tp+fp);
+    recall = tp/(tp+fn);
+    ps(m) = precision;
+    rs(m) = recall;
+end
+
+% plot the precision-recall curve
+figure
+plot(rs, ps); axis([0 1 0 1])
+
